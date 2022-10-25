0a704ecc7db768e1258388e52985fb31da47c19b
xanax <34755589+anKordii@users.noreply.github.com>
2022-10-25 19:50:23 +0200

    Update Censor.js

--- functions/Censor.js
+++ functions/Censor.js
@@ -1,7 +1,7 @@
 export default function Censored(text) {
     var regex = /nigger|nugger|pedał|czarnuch|killjews|sex|vagina|whore|hopeyoudie|pedal/gi;
     
-    var str = text.toLowerCase();
+    var str = text;
 
     str = str.replace(regex, "xspeedyq");
 
