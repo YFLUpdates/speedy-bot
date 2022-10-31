529ca14aef334de6e7869511fd84e903507e72d6
xanax <34755589+anKordii@users.noreply.github.com>
2022-10-31 19:38:32 +0100

    Update get5City.js

--- functions/fivem/get5City.js
+++ functions/fivem/get5City.js
@@ -7,14 +7,18 @@ export default async function getFiveCity(user) {
     const fivecity = await cfx.fetchServer("ggoe6z");
     const onServer = fivecity.players.length;
     let streamers = 0;
+    let random = [];
 
     await Promise.all(
         fivecity.players.map(async (i, index) => {
             if(znaniHex.includes(i.identifiers[0])){
+                if(random.length < 3){
+                    random.push(i.name.toLowerCase())
+                }
                 streamers += 1;
             }
         })
     )
 
-    return `${user} na 5City aktualnie jest ${onServer} osób, z czego ${streamers} to znane osoby jasperTragedia `
+    return `jasperTragedia ${user} na 5City aktualnie jest ${onServer} osób, z czego ${streamers} to znane osoby np. ${random.join(", ")} `
 }
