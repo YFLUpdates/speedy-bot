d0c5450429d8344e17ab8ea7ee161dc7c138e695
xanax <34755589+anKordii@users.noreply.github.com>
2022-10-31 21:04:32 +0100

    Update zaprasza.js

--- commands/zaprasza.js
+++ commands/zaprasza.js
@@ -5,21 +5,21 @@ export default async function hugC(channel, username, argument){
     const usernameSmall = username.toLowerCase();
 
     if(argument && argument !== " "){
-        return `${usernameSmall} zaprasza na kamerki ${Censor(argument)} jasperM  `;
+        return `${usernameSmall} zaprasza ${Censor(argument)} na kamerki jasperM  `;
     }else{
         return await getRandomChatter(channel, { skipList: [ usernameSmall ] })
         .then(user => {
             if(user === null) {
-                return `${usernameSmall} zaprasza na kamerki YFLUpdates jasperLaskotanie  `;
+                return `${usernameSmall} zaprasza YFLUpdates na kamerki jasperLaskotanie  `;
             }
             else {
                 let { name } = user;
-                return `${usernameSmall} zaprasza na kamerki ${name} jasperM  `;
+                return `${usernameSmall} zaprasza ${name} na kamerki jasperM  `;
             }
         })
         // .catch(err => console.log(err));
         .catch(err => {
-            return `${usernameSmall} zaprasza na kamerki YFLUpdates jasperLaskotanie  `;
+            return `${usernameSmall} zaprasza YFLUpdates na kamerki jasperLaskotanie  `;
         });
     }
 }
