f8fd1454d6b2373de09ddf3a0539a6e921668d0d
xanax <34755589+anKordii@users.noreply.github.com>
2022-10-31 21:31:40 +0100

    5city random players

--- functions/fivem/get5City.js
+++ functions/fivem/get5City.js
@@ -1,5 +1,6 @@
 import cfx from "cfx-api";
 import { promises as fs } from 'fs';
+import getMultipleRandom from "../../components/getMultipleRandom.js";
 
 const znaniHex = JSON.parse(await fs.readFile('./steam_hex.json', 'UTF-8'))
 
@@ -7,18 +8,17 @@ export default async function getFiveCity(user) {
     const fivecity = await cfx.fetchServer("ggoe6z");
     const onServer = fivecity.players.length;
     let streamers = 0;
-    let random = [];
+    let streamrsArray = [];
 
     await Promise.all(
         fivecity.players.map(async (i, index) => {
             if(znaniHex.includes(i.identifiers[0])){
-                if(random.length < 3){
-                    random.push(i.name.toLowerCase())
-                }
+                streamrsArray.push(i.name.toLowerCase())
                 streamers += 1;
             }
         })
     )
+    const random = getMultipleRandom(streamrsArray, 3);
 
     return `jasperTragedia ${user} na 5City aktualnie jest ${onServer} osób, z czego ${streamers} to znane osoby np. ${random.join(", ")} `
 }
