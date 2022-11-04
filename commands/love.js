82a17070efd9abd636ffb1ca97ece55676a4bb1f
xanax <34755589+anKordii@users.noreply.github.com>
2022-11-04 20:44:12 +0100

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -8,7 +8,7 @@ export default async function hugC(channel, username, argument){
 
         return `${usernameSmall} jest zakochany(a) na ${randomNumber(0, 100)}% w ${usernameSmall} <3  `;
     }else if(argument){
-        return `${usernameSmall} jest zakochany(a) na ${randomNumber(0, 100)}% w ${Censor(argument)}<3  `;
+        return `${usernameSmall} jest zakochany(a) na ${randomNumber(0, 100)}% w ${Censor(argument)} <3  `;
     }else{
         return `${usernameSmall} jest zakochany(a) na ${randomNumber(0, 100)}% w ${usernameSmall} <3  `;
     }
