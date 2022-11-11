da24a8d4f3d6609a6f5719f36111262284f347df
xanax <34755589+anKordii@users.noreply.github.com>
2022-11-11 20:59:37 +0100

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -1,4 +1,5 @@
 import {Censor} from "../functions/index.js";
+import {getRandomChatter} from "../functions/index.js"
 import {randomNumber} from "../functions/index.js";
 import humanizeDuration from "humanize-duration";
 
@@ -9,8 +10,21 @@ export default async function hugC(channel, username, argument){
 
         return `${usernameSmall} nigdy nie weźmie ślubu xd `;
     }else if(argument){
-        return `${usernameSmall} weźmie ślub z ${Censor(argument)} za ${humanizeDuration(randomNumber(1440, 1051200) * 60000, { language: "pl" })} jupijej  `;
+        return `${usernameSmall} weźmie ślub z ${Censor(argument)} za ${humanizeDuration(randomNumber(1440, 1051200) * 60000, { language: "pl" })} jupijej `;
     }else{
-        return `${usernameSmall} ma w planach wzięcie ślubu z samym sobą aha `;
+        return await getRandomChatter(channel, { skipList: [ usernameSmall ] })
+        .then(user => {
+            if(user === null) {
+                return `${usernameSmall} weźmie ślub z ${channel} za ${humanizeDuration(randomNumber(1440, 1051200) * 60000, { language: "pl" })} jupijej `;
+            }
+            else {
+                let { name } = user;
+                return `${usernameSmall} weźmie ślub z ${name} za ${humanizeDuration(randomNumber(1440, 1051200) * 60000, { language: "pl" })} jupijej `;
+            }
+        })
+        // .catch(err => console.log(err));
+        .catch(err => {
+            return `${usernameSmall} weźmie ślub z ${channel} za ${humanizeDuration(randomNumber(1440, 1051200) * 60000, { language: "pl" })} jupijej `;
+        });
     }
 }
