1a327cdcb0bd23a2b35f94261946cdc2df4b5280
xan <34755589+anKordii@users.noreply.github.com>
2022-12-06 22:09:36 +0100

    Update offlinetime.js

--- commands/offlinetime.js
+++ commands/offlinetime.js
@@ -1,11 +1,10 @@
-import {Censor} from "../functions/index.js";
 import {offlineTime} from "../functions/requests/index.js";
 
 export default async function hugC(channel, username, argument){
     const usernameSmall = username.toLowerCase();
 
     if(argument && argument.length > 3){
-        const channels = await offlineTime(Censor(argument, channel));
+        const channels = await offlineTime(argument);
 
         return channels;
     }else{
