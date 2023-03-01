a0a8ea36d676e3eb337b404f6ce3218bbd464147
xanax <34755589+anKordii@users.noreply.github.com>
2023-03-01 01:25:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,10 +116,10 @@ setInterval(async () => {
           array.includes("[YFL SMP]") ||
           array.includes("[SMP]") ||
           array.includes("[YFL") ||
-          array.includes("SMP]" ||
+          array.includes("SMP]") ||
           array.includes("GILDIA") ||
           array.includes("WALKA") ||
-          array.includes("WOJNA"))
+          array.includes("WOJNA")
         ) {
           streams.streams.push({
             nickname: e.user_name,
@@ -157,7 +157,10 @@ setTimeout(async () => {
             array.includes("[YFL SMP]") ||
             array.includes("[SMP]") ||
             array.includes("[YFL") ||
-            array.includes("SMP]")
+            array.includes("SMP]") ||
+            array.includes("GILDIA") ||
+            array.includes("WALKA") ||
+            array.includes("WOJNA")
           ) {
             streams.streams.push({
               nickname: e.user_name,
