8cb8710def3f62b363020cdb4db576f981bba5cb
xanax <34755589+anKordii@users.noreply.github.com>
2023-03-21 19:14:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1287,6 +1287,10 @@ client.on('message', async (channel, tags, message, self) => {
             return client.say(channel, `${cleanSender}, rose - 1k, coffe - 2,5k, koniczynka - 5k, kiss - 7,5k, szampan - 10k, diamonds - 20k, duck - 30k, dice - 50k, lean - 100k, ziolo - 110k -> https://yfl.es/alerts.png `); 
         }
 
+        if(argumentClean === "alert"){
+            return client.say(channel, `${cleanSender}, autorem alertów jest @Majhel FIRE GagriGagri `); 
+        }
+
         if(!["coffe", "diamonds", "dice", "kiss", "koniczynka", "rose", "szampan", "lean", "ziolo", "duck"].includes(argumentClean)){
             return client.say(channel, `${cleanSender}, nieznany rodzaj -> !tiktok list aha`); 
         }
